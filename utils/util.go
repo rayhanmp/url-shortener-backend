07f0efb10147b1fc06273a2021e81d9ad2c18ccd
Rayhan Maheswara Pramanda <90276351+rayhanmp@users.noreply.github.com>
2024-05-15 07:23:53 +0700

    style(utils): change variable name from b to shortCode

--- utils/util.go
+++ utils/util.go
@@ -14,9 +14,9 @@ const (
 var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func StringWithCharset(length int, charset string) string {
-	b := make([]byte, length)
-	for i := range b {
-		b[i] = charset[seededRand.Intn(len(charset))]
+	shortCode := make([]byte, length)
+	for i := range shortCode {
+		shortCode[i] = charset[seededRand.Intn(len(charset))]
 	}
-	return string(b)
+	return string(shortCode)
 }
